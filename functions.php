2f1f8f83f049336cce42472443b5836264f5174e
Valerio <valeriozarba@gmail.com>
2023-02-23 23:25:52 +0100

    Primo commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+	$parenthandle = 'twentytwentyone'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	$theme        = wp_get_theme();
+	wp_enqueue_style( $parenthandle,
+		get_template_directory_uri() . '/style.css',
+		array(),  // If the parent theme code has a dependency, copy it to here.
+		$theme->parent()->get( 'Version' )
+	);
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_uri(),
+		array( $parenthandle ),
+		$theme->get( 'Version' ) // This only works if you have Version defined in the style header.
+	);
+	$my_js_ver  = date("ymd-Gis", filemtime( get_stylesheet_directory_uri().'/build/main.js'));
+	wp_enqueue_script( 'main-child', get_stylesheet_directory_uri().'/build/main.js', array(), $my_js_ver );
+}
